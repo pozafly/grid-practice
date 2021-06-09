fa99526e8a216c700b1608baf0047d4e9525451f
HwangSunTae <pozafly@gmail.com>
2021-06-09 17:42:23 +0900

    init

---
+++ src/views/MainPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="color-red">
+    MainPagedd
+    <BoilerPlateComponent msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import BoilerPlateComponent from '@/components/BoilerPlateComponent.vue';
+
+export default {
+  components: {
+    BoilerPlateComponent,
+  },
+};
+</script>
+
+<style lang="scss">
+$bgColor: red;
+
+.color-red {
+  color: $bgColor;
+}
+</style>
